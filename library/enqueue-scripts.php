c1c9291d8b77a0d6ef5282bbda70512b42b37985
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-02-12 16:37:02 -0600

    added some pages and styled a lot

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -12,23 +12,24 @@
 if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
-	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.9.0', 'all' );
+		// Enqueue the main Stylesheet.
+		wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.9.0', 'all' );
+		wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Bevan|Source+Sans+Pro:300,400,600', array());
 
-	// Deregister the jquery version bundled with WordPress.
-	wp_deregister_script( 'jquery' );
+		// Deregister the jquery version bundled with WordPress.
+		wp_deregister_script( 'jquery' );
 
-	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+		// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+		wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
-	// If you'd like to cherry-pick the foundation components you need in your project, head over to gulpfile.js and see lines 35-54.
-	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.9.0', true );
+		// If you'd like to cherry-pick the foundation components you need in your project, head over to gulpfile.js and see lines 35-54.
+		// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
+		wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.9.0', true );
 
-	// Add the comment-reply library on pages where it is necessary
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+		// Add the comment-reply library on pages where it is necessary
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
 
 	}
 
--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,7 +1,18 @@
 <?php
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
-if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header id="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
-	</header>
-<?php endif;
+if ( has_post_thumbnail( $post->ID ) ) { 
+    $id = $post->ID;
+} else {
+    $home = get_page_by_path('learn-stuff-build-things');
+    $id = $home->ID;
+} ?>
+
+    <header id="featured-hero" role="banner" data-interchange="[<?php echo get_the_post_thumbnail_url($id, 'featured-small'); ?>, small], [<?php echo get_the_post_thumbnail_url($id, 'featured-medium'); ?>, medium], [<?php echo get_the_post_thumbnail_url($id, 'featured-large'); ?>, large], [<?php echo get_the_post_thumbnail_url($id, 'featured-xlarge'); ?>, xlarge]">
+        <div class="overlay">
+            <div class="chaperone">
+                <?php the_custom_logo(); ?>
+            </div>
+        </div>
+    </header>
+<?php 
