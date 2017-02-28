d4639eca7983c4f80801c9577bd9ffabcb0b9ba0
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-02-27 21:24:43 -0600

    edited and ditched maps template

--- page-templates/map.php
+++ page-templates/map.php
@@ -9,7 +9,13 @@ get_header(); ?>
 <div id="page-full-width" class="faq" role="main">
   <?php echo $post->post_content; ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-    <div id="map">
+    <div id="legend" class="large-3 medium-4 small-12 columns">
+      <div class="legend blue"></div>
+      <p>Storage</p>
+      <div class="legend red"></div>
+      <p>Metal shop</p>
+    </div>
+    <div id="map" class="large-9 medium-8 small-12 columns">
     </div>
   </article>
 
@@ -22,21 +28,53 @@ get_header(); ?>
         scrollwheel: false,
       });
       var makerspaceCoords = [
-        {lat: 36.176709, lng: -86.753075},
-        {lat: 36.176507, lng: -86.752958},
-        {lat: 36.176449, lng: -86.753123},
-        {lat: 36.176548, lng: -86.753177},
-        {lat: 36.176519, lng: -86.753260},
-        {lat: 36.176568, lng: -86.753288},
-        {lat: 36.176534, lng: -86.753385},
-        {lat: 36.176586, lng: -86.753412},
-        {lat: 36.176709, lng: -86.753075}
+        {lat: 36.176709, lng: -86.753076}, // top right
+        {lat: 36.176507, lng: -86.752958}, // bottom right
+        {lat: 36.176449, lng: -86.753123}, // bottom sub-right
+        {lat: 36.176565, lng: -86.753187}, // inside top right
+        {lat: 36.176504, lng: -86.753368}, // sub-bottom left
+        {lat: 36.176555, lng: -86.753396}, // sub-top left
+        {lat: 36.176566, lng: -86.753364}, // inside bottom right
+        {lat: 36.176597, lng: -86.753381}, // top sub-left
+        {lat: 36.176709, lng: -86.753076} // top right
       ];
       var makerspace = new google.maps.Polygon({
         path: makerspaceCoords,
         map: map,
-        strokeOpacity: .2,
+        strokeOpacity: .3,
         fillOpacity: 0,
+      });
+
+      // metal shop
+      var metalshopCoords = [
+        {lat: 36.176619, lng: -86.753317}, // top left
+        {lat: 36.176649, lng: -86.753233}, // top right
+        {lat: 36.176565, lng: -86.753187}, // bottom right
+        {lat: 36.176535, lng: -86.753272}, // bottom left
+      ];
+      var metalshop = new google.maps.Polygon({
+        path: metalshopCoords,
+        map:map,
+        strokeOpacity: 0,
+        fillOpacity: .2,
+        fillColor: '#33cc33',
+      });
+
+      // metal shop
+      var storageCoords = [
+        {lat: 36.176619, lng: -86.753317}, // top right
+        {lat: 36.176535, lng: -86.753272}, // bottom right
+        {lat: 36.176504, lng: -86.753368}, // bottom left
+        {lat: 36.176555, lng: -86.753396}, // sub-top left
+        {lat: 36.176566, lng: -86.753364}, // inside bottom right
+        {lat: 36.176597, lng: -86.753381}, // top sub-left
+      ];
+      var storage = new google.maps.Polygon({
+        path: storageCoords,
+        map:map,
+        strokeOpacity: 0,
+        fillOpacity: .2,
+        fillColor: '#3333ff',
       });
     }
 
