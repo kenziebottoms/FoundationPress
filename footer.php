89fe6259285f3d86753e0547ec1463383aaeafdb
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-03-22 23:45:30 -0500

    fixed social links footer right up

--- footer.php
+++ footer.php
@@ -13,13 +13,33 @@
 		</section>
 		<div id="footer-container" class="row">
 			<div class="large-6 medium-6 small-12 columns">
-				<?php $fb = get_option('facebook');
-				$insta = get_option('instagram');
-				$tw = get_option('twitter'); ?>
-				<?php if ($fb != '') { ?>
-					<a href='<?php echo $fb; ?>'>Facebook</a>
-				<?php } ?>
 				<?php dynamic_sidebar( 'footer-widgets' ); ?>
+				<ul id="social">
+					<?php $fb = get_option('facebook');
+					$insta = get_option('instagram');
+					$tw = get_option('twitter'); ?>
+					<?php if ($fb != '') { ?>
+						<li>
+							<a target="blank" href='<?php echo $fb; ?>'>
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/fb.png" />
+							</a>
+						</li>
+					<?php } ?>
+					<?php if ($tw != '') { ?>
+						<li>
+							<a target="blank" href='<?php echo $tw; ?>'>
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/tw.png" />
+							</a>
+						</li>
+					<?php } ?>
+					<?php if ($insta != '') { ?>
+						<li>
+							<a target="blank" href='<?php echo $insta; ?>'>
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/insta.png" />
+							</a>
+						</li>
+					<?php } ?>
+				</ul>
 			</div>
 			<div class="large-6 medium-6 small-12 columns">
 				<?php dynamic_sidebar( 'map' ); ?>
